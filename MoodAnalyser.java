4f1d627bd1644d468fcfe87bb83d23e43ec1c092
Srikar <srikarsamanchi1999@gmail.com>
2021-06-27 10:17:10 +0530

    Added : Mood analyser Uc2

--- MoodAnalyser.java
+++ MoodAnalyser.java
@@ -2,11 +2,26 @@ package com.moodAnalyser;
 
 public class MoodAnalyser {
 
-    public String analyseMood(String message) {
-        //Checking for SAD
-        if(message.contains("SAD"))
-            return "SAD";
-        else
+    private String message;
+
+    //Parameterized Constructor
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    public String MoodAnalyser(String message) {
+        this.message = message;
+        return analyseMood();
+    }
+    
+    public String analyseMood() {
+        try {
+            if (message.contains("SAD"))
+                return "SAD";
+            else
+                return "HAPPY";
+        } catch (NullPointerException e) {
             return "HAPPY";
+        }
     }
 }
